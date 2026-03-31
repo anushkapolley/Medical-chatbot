529c996755027e5b2d32a639fea8e8627e33238d
anushkapolleyVE <anushkapolley@virtualemployee.com>
2026-03-31 12:18:21 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,20 @@
+#creating directories
+mkdir -p src
+mkdir -p resrarch
+
+echo "Directory created successfully"
+#creating files 
+#constructor file 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch resrarch/trial.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Files created successfully"
+
+echo "Directory and files created successfully"
